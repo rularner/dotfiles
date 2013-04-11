d942dec36898e16fc1d650692a97f5672586219b
Rusty Larner <rularner@imvu.com>
2013-04-11 09:13:45 -0700

    Initial config, using oh-my-zsh

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,8 @@
 # GRC colorizes nifty unix tools all over the place
 if $(grc &>/dev/null)
 then
-  source `brew --prefix`/etc/grc.bashrc
+  if [[ $(uname -s) == "Darwin" ]]
+  then
+    source `brew --prefix`/etc/grc.bashrc
+  fi
 fi
